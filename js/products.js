05fda7297eeee9404ebb45ba10c92d0c08b5c16a
CLaura155 <chikan.laura@gmail.com>
2026-03-02 18:17:05 +0100

    Storing products in shopping basket
    Using localStorage

--- js/products.js
+++ js/products.js
@@ -1,21 +1,76 @@
-const aElementsArray = document.getElementsByClassName("img-in-grid");
+const imgElementsArray = document.getElementsByClassName("artwork-img");
 const cardBoxesArray = document.getElementsByClassName("title-card");
 const cardTitlesArray = document.getElementsByClassName("title-card-text");
 
-for (i = 0; i < aElementsArray.length; i++) {
-  let aElement = aElementsArray[i];
+for (i = 0; i < imgElementsArray.length; i++) {
+  let imgElement = imgElementsArray[i];
   let titleCardBox = cardBoxesArray[i];
   let titleCardText = cardTitlesArray[i];
-  aElement.addEventListener("mouseover", showTitle);
+  imgElement.addEventListener("mouseover", showTitle);
 
   function showTitle() {
     titleCardBox.style.display = "block";
     titleCardText.style.display = "block";
 
-    aElement.addEventListener("mouseout", disappearTitle);
+    imgElement.addEventListener("mouseout", disappearTitle);
     function disappearTitle() {
       titleCardBox.style.display = "none";
       titleCardText.style.display = "none";
     }
   }
+}
+
+let basketIcons = document.getElementsByClassName("basket-outline");
+let basketArr;
+
+if (localStorage.basketItems) {
+  basketArr = JSON.parse(localStorage.basketItems);
+} else {
+  basketArr = [];
+}
+
+for (let i = 0; i < basketIcons.length; i++) {
+  for (let j = 0; j < basketArr.length; j++) {
+    if (basketIcons[i].dataset.id === basketArr[j].id) {
+      if (basketIcons[i].src.includes("gray")) {
+        basketIcons[i].src = "img/icons/basket-gray-filled.svg";
+      } else {
+        basketIcons[i].src = "img/icons/basket-filled.svg";
+      }
+    }
+  }
+}
+
+for (let i = 0; i < basketIcons.length; i++) {
+  basketIcons[i].addEventListener("click", saveToBasket);
+}
+
+function saveToBasket() {
+  let basketItem = {
+    id: this.dataset.id,
+    img: this.dataset.img,
+  };
+
+  for (let i = 0; i < basketArr.length; i++) {
+    if (this.dataset.id === basketArr[i].id) {
+      basketArr.splice(i, 1);
+    }
+  }
+
+  if (this.src.includes("outline")) {
+    if (this.src.includes("gray")) {
+      this.src = "img/icons/basket-gray-filled.svg";
+    } else {
+      this.src = "img/icons/basket-filled.svg";
+    }
+    basketArr.push(basketItem);
+  } else {
+    if (this.src.includes("gray")) {
+      this.src = "img/icons/basket-gray-outline.svg";
+    } else {
+      this.src = "img/icons/basket-outline.svg";
+    }
+  }
+
+  localStorage.basketItems = JSON.stringify(basketArr);
 }
