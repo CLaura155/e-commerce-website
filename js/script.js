ee1cd5ff4d9d40d2759c3dc755ae56303897bc88
CLaura155 <chikan.laura@gmail.com>
2026-03-09 15:35:44 +0100

    Added: basket icon is filled when hovered

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 let menuIcon = {};
 let mainPageContent = document.querySelector("main");
 let menuOverlay = document.querySelector("menu");
+let basketIcon = document.querySelector(".basket-icon");
 
 if (document.querySelector("#hamburger-menu-icon")) {
   menuIcon.element = document.querySelector("#hamburger-menu-icon");
   menuIcon.type = "hamburger-menu";
-  console.log("Menu icon exists");
-  console.log(menuIcon.type);
 }
 
 menuIcon.element.addEventListener("click", openCloseMenu);
+basketIcon.addEventListener("mouseenter", filledIcon);
+basketIcon.addEventListener("mouseleave", outlineIcon);
+
+function filledIcon() {
+  basketIcon.src = "/img/icons/basket-filled.svg";
+}
+function outlineIcon() {
+  basketIcon.src = "/img/icons/basket-outline.svg";
+}
 
 function openCloseMenu() {
   if (menuIcon.type === "hamburger-menu") {
     menuIcon.type = "close-icon";
     menuIcon.element.src = "/img/icons/close-icon.svg";
-    console.log("icon changed to X");
     mainPageContent.style.display = "none";
     menuOverlayStyling();
   } else {
     menuIcon.type = "hamburger-menu";
     menuIcon.element.src = "/img/icons/hamburger-menu-icon.svg";
-    console.log("icon changed to Hambi");
     mainPageContent.style.display = "block";
     menuOverlay.style.display = "none";
   }
